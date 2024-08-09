c14107c99f81b2e972c66471e41d9bc44ee32db6
Vishal Kurve <vishalkurve7@gmail.com>
2024-08-09 18:46:33 +0530

    Update Maximize Array Value After Rearrangement.cpp

--- Maximize Array Value After Rearrangement.cpp
+++ Maximize Array Value After Rearrangement.cpp
@@ -1,12 +1,16 @@
 // Maximize Array Value After Rearrangement
 // GFG : Easy 09-08-2024
 
+class Solution {
+  public:
 int Maximize(vector<int> &arr) {
     int mod = 1e9 + 7;
     sort(begin(arr), end(arr));
-    long long sum = 0; 
+    long long sum = 0; // Use long long to prevent overflow
     for (int i = 0; i < arr.size(); i++) {
-        sum = (sum + ((long long)arr[i]) * i) % mod) % mod;
+        sum = (sum + (static_cast<long long>(arr[i]) * i) % mod) % mod; // Ensure each multiplication is modded
     }
     return sum;
 }
+
+};
