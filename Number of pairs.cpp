25a88b92f3591f5b3cb592a67e39f223e3ca432d
Vishal Kurve <vishalkurve7@gmail.com>
2024-08-26 22:51:22 +0530

    Create Number of pairs.cpp
    https://www.geeksforgeeks.org/problems/number-of-pairs-1587115620/1

---
+++ Number of pairs.cpp
@@ -0,0 +1,38 @@
+// Number of pairs
+// GFG : Medium 26-08-2024
+
+class Solution {
+  public:
+    int binarySearch(int key,vector<int> &brr){
+        int low=0,high=brr.size()-1,mid,index=-1;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(brr[mid]<=key){
+                index=mid;
+                low=mid+1;
+            }
+            else high=mid-1;
+        }
+        return index;
+    }
+    long long countPairs(vector<int> &arr, vector<int> &brr) {
+        // Your Code goes here.
+        long long ans = 0, one = 0, two = 0, threeFour = 0;
+        sort(brr.begin(),brr.end());
+        for(auto x:brr){
+            if(x==1)one++;
+            else if(x==2)two++;
+            else if(x==3 or x==4)threeFour++;
+        }
+        for(auto x:arr){
+            if(x!=1){
+                ans+=one;
+                if(x==2)ans-=threeFour;
+                if(x==3)ans+=two;
+                int index = binarySearch(x,brr);
+                ans+=(brr.size()-index-1);
+            }
+        }
+        return ans;
+    }
+};
